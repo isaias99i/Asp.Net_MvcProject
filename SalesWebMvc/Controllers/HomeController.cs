13afe80b777c00db8e460c21e4d2d82c5e847ffe
isaias99i <isaiascarlos99i@gmail.com>
2022-07-31 23:49:49 -0300

    First Controller and Razor page Tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC APP from C# Course.";
+            ViewData["Email"] = "isaias@gmail.com";
 
             return View();
         }
